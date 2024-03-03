cc242877a13b2aefe674e66b94045d941e8991fd
FabianAndresHuertasCano <fabian.huertas-c@uniminuto.edu.co>
2024-03-03 13:12:09 -0500

    Republicación de rama develop
    Republicación de rama develop

--- PAGINA_WEB/TARJETA_PRESENTACION/Sources/Pages/Admin.aspx.designer.cs
+++ PAGINA_WEB/TARJETA_PRESENTACION/Sources/Pages/Admin.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace TARJETA_PRESENTACION.Sources.Pages
     {
 
         /// <summary>
+        /// Control hidenId.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox hidenId;
+
+        /// <summary>
         /// Control tbIdProducto.
         /// </summary>
         /// <remarks>
